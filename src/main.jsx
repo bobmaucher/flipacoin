74778d2c3f253fbeb40adee9fc6e128b18d152d4
Bob Maucher <bobmaucher@gmail.com>
2025-04-27 11:43:32 -0400

    Add Google Analytics integration and minor updates

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
+import ReactGA from "react-ga4";
+
+// Initialize Google Analytics
+ReactGA.initialize("G-FQL6MZ4T39");  // <-- Your ID here
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
